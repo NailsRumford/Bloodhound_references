cd7d0ad0bedf04dab80bcecdaa22120f03dde602
Артем Волков <NailsRumford@internet.ru>
2023-07-01 17:53:00 +0300

    add logging

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 import sys
+import os
+import logging
 from utils.image_finder import ImageFinder
 from utils.sequence_sorter import SequenceSorter
 from utils.video_creator import VideoCreator
 
-if __name__ == '__main__':
+def main():
     if len(sys.argv) != 2:
-        print("Usage: python script.py <folder_path>")
-    else:
-        folder_path = sys.argv[1]
-    #folder_path = '/home/nailsrumford/Dev/Bloodhound_references/test'
-    images = ImageFinder(folder_path).find_images()
+        logging.error("Usage: python main.py <folder_path>")
+        return
+
+    folder_path = sys.argv[1]
+
+    if not os.path.isdir(folder_path):
+        logging.error(f"Invalid folder path: {folder_path}")
+        return
+
+    image_finder = ImageFinder(folder_path)
+    images = image_finder.find_images()
+
+    if not images:
+        logging.error("No images found in the specified folder.")
+        return
+
     sorted_sequences = SequenceSorter.sort_sequences(images)
+
+    if not sorted_sequences:
+        logging.error("No image sequences found.")
+        return
+
     output_folder = folder_path
+
     VideoCreator.create_videos(sorted_sequences, output_folder)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
