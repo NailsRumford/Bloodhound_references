3aabab2af1ecd6099bcaea38b86de05b7322387a
Артем Волков <NailsRumford@internet.ru>
2023-07-01 17:45:26 +0300

    add logging

--- utils/sequence_sorter.py
+++ utils/sequence_sorter.py
@@ -1,5 +1,6 @@
 import os
 from typing import Dict, List, Tuple
+import logging
 
 class SequenceSorter:
     """
@@ -19,30 +20,31 @@ class SequenceSorter:
                 Each sequence is represented as a tuple containing name, padding, frames.
                 Each frame is represented as a tuple containing frame number, image path, padding.
         """
+        if not images:
+            logging.warning("No images found. Empty list provided.")
+            return []
+
         sequences: Dict[str, List[Tuple[int, str, int]]] = {}
 
-        try:
-            for image_path in images:
-                filename = os.path.basename(image_path)
-                name, frame, padding = SequenceSorter._parse_filename(filename)
+        for image_path in images:
+            filename = os.path.basename(image_path)
+            name, frame, padding = SequenceSorter._parse_filename(filename)
 
-                if name in sequences:
-                    sequences[name].append((frame, image_path, padding))
-                else:
-                    sequences[name] = [(frame, image_path, padding)]
+            if name in sequences:
+                sequences[name].append((frame, image_path, padding))
+            else:
+                sequences[name] = [(frame, image_path, padding)]
 
-            sorted_sequences = []
-            for name, frames in sequences.items():
-                frames.sort(key=lambda x: x[0])
-                padding = frames[0][2]
-                output_filename = name + '.mov'
-                first_frame_path = frames[0][1]
-                folder_path = os.path.dirname(first_frame_path)
-                sorted_sequences.append((name, padding, frames, output_filename, folder_path))
+        sorted_sequences = []
+        for name, frames_list in sequences.items():
+            frames_list.sort(key=lambda x: x[0])
+            padding = frames_list[0][2]
+            output_filename = name + '.mov'
+            first_frame_path = frames_list[0][1]
+            folder_path = os.path.dirname(first_frame_path)
+            sorted_sequences.append((name, padding, frames_list, output_filename, folder_path))
 
-            return sorted_sequences
-        except ValueError as e:
-            print(f"Error occurred while sorting sequences: {e}")
+        return sorted_sequences
 
     @staticmethod
     def _parse_filename(filename: str) -> Tuple[str, int, int]:
